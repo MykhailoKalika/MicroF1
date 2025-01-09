07b583884e68af5b02ace5120349291df50f8c95
mykhailoKalika <mykhailo.kalika@swansoftwaresolutions.com>
2025-01-09 16:39:56 +0200

    add about me page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {AboutMeComponent} from './pages/about-me/about-me.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AboutMeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/pages/about-me/about-me.component.ts
@@ -0,0 +1,38 @@
+import {Component} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {NgIf} from '@angular/common';
+
+@Component({
+  selector: 'app-about-me',
+  imports: [
+    NgIf,
+    ReactiveFormsModule,
+  ],
+  templateUrl: './about-me.component.html',
+  styleUrl: './about-me.component.scss'
+})
+export class AboutMeComponent {
+
+  userForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.userForm = this.fb.group({
+      firstName: new FormControl<string>('', [Validators.required]),
+      lastName: new FormControl<string>('', [Validators.required]),
+      dob: new FormControl<string>('', [Validators.required]),
+      email: new FormControl<string>('', [Validators.required, Validators.email]),
+      phone: new FormControl<string>('', [Validators.required, Validators.pattern(/^\+?\d{10,12}$/)])
+    });
+  }
+
+  onSubmit(event: Event): void {
+    this.userForm.markAllAsTouched();
+    event.preventDefault();
+
+    if (this.userForm.valid) {
+      console.log('Form Submitted!', this.userForm.value);
+    } else {
+      console.log('Form is invalid!');
+    }
+  }
+}
