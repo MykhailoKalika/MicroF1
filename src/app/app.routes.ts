b6f83e42398da45308e8cf26bb9f27da61e58b01
mykhailoKalika <mykhailo.kalika@swansoftwaresolutions.com>
2025-01-09 16:47:02 +0200

    Configured routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import {AboutMeComponent} from './pages/about-me/about-me.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'about_me', component: AboutMeComponent },
+  { path: '**', redirectTo: 'about_me', pathMatch: 'full' }
+];
