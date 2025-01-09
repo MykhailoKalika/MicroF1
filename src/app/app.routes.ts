bcf15cef79b76d24bceffbf76d04635bbc9cabb0
mykhailoKalika <mykhailo.kalika@swansoftwaresolutions.com>
2025-01-09 17:38:03 +0200

    Added higher education page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import {AboutMeComponent} from './pages/about-me/about-me.component';
 import {EducationComponent} from './pages/education/education.component';
+import {HigherEducationComponent} from './pages/higher-education/higher-education.component';
 
 export const routes: Routes = [
   { path: 'about_me', component: AboutMeComponent },
   { path: 'education', component: EducationComponent },
+  { path: 'higher-education', component: HigherEducationComponent },
   { path: '**', redirectTo: 'about_me', pathMatch: 'full' }
 ];
---
+++ src/app/pages/higher-education/higher-education.component.ts
@@ -0,0 +1,55 @@
+import { Component } from '@angular/core';
+import {FormArray, FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
+import {NgForOf, NgIf} from '@angular/common';
+
+@Component({
+  selector: 'app-higher-education',
+  imports: [
+    FormsModule,
+    NgForOf,
+    NgIf,
+    ReactiveFormsModule
+  ],
+  templateUrl: './higher-education.component.html',
+  styleUrl: './higher-education.component.scss',
+})
+export class HigherEducationComponent {
+
+  userForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.userForm = this.fb.group({
+      education: this.fb.array([this.createEducationBlock()]),
+    });
+  }
+
+  createEducationBlock(): FormGroup {
+    return this.fb.group({
+      institution: ['', Validators.required],
+      degree: ['', Validators.required],
+      startYear: ['', [Validators.required, Validators.min(1900), Validators.max(new Date().getFullYear())]],
+      endYear: ['', [Validators.required, Validators.min(1900), Validators.max(new Date().getFullYear())]]
+    });
+  }
+
+  get educationBlocks(): FormArray {
+    return this.userForm.get('education') as FormArray;
+  }
+
+  addBlock(): void {
+    this.educationBlocks.push(this.createEducationBlock());
+  }
+
+  removeBlock(index: number): void {
+    this.educationBlocks.removeAt(index);
+  }
+
+  onSubmit(): void {
+    this.userForm.markAllAsTouched();
+
+    if (this.userForm.valid) {
+      this.router.navigate(['/work-experience']);
+    }
+  }
+}
